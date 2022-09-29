da7690095a490f3daf071d5455fe66841360f35a
Zephyrus011 <ritvikgupta011@gmail.com>
2022-09-29 19:36:29 -0400

    init, app layout completed

---
+++ app/src/main/java/com/app/kiosk/MainActivity.java
@@ -0,0 +1,28 @@
+package com.app.kiosk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public String inputCode = "";
+    // TextView textView = (TextView) findViewById(R.id.textBox);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        textView.setText(inputCode);
+    }
+
+    Button oneButton = (Button) findViewById(R.id.one);
+    oneButton.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View view) {
+            inputCode += "1";
+        }
+    });
+}
